bb0191f3ba4b71016c1b6b6193b22b55c9e51367
bisforbunny <108408276+bisforbunny@users.noreply.github.com>
2022-06-30 16:31:21 -0700

    Update probe.ps1

--- scripts/probe.ps1
+++ scripts/probe.ps1
@@ -1,3 +1,3 @@
 dir C:\hostedtoolcache\windows\CodeQL\0.0.0-20220615\x64\codeql\csharp\tools
 
-get-childitem -recurse C:\hostedtoolcache\windows\CodeQL\0.0.0-20220615\x64\codeql\csharp\tools
+get-childitem -recurse C:\hostedtoolcache\windows\CodeQL\0.0.0-20220615\x64\codeql\qlpacks\codeql
